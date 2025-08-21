c209614b38f23dae48a4356b38b9845ad61cd36b
secure-server-eng <rayzarr9@gmail.com>
2025-08-21 11:55:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const submitRequest = require("./routes/submitRequest");
@@ -6,3 +7,4 @@ app.use(express.json());
 app.use("/submit-request", submitRequest);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
+
